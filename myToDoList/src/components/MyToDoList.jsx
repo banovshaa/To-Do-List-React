6aec608db45983e8e59794e496749f91d5152767
banovshaa <banovshaea@code.edu.az>
2023-01-26 21:23:08 +0400

    something added

--- myToDoList/src/components/MyToDoList.jsx
+++ myToDoList/src/components/MyToDoList.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Task from './Task'
 
 const MyToDoList = () => {
+
     const[task,setTask]=useState('')
     const[toDoList,setToDoList]=useState([])
 
@@ -10,7 +11,8 @@ const MyToDoList = () => {
     const addHandler=()=>{
         const newTask={
             Id:toDoList.length===0?1:(toDoList[toDoList.length-1].Id)+1,
-            Name:task
+            Name:task,
+            Completed:false
         }
         setToDoList([...toDoList,newTask])
     }
@@ -19,19 +21,30 @@ const MyToDoList = () => {
            return l.Id!==id
         }))
     }
+    const isCompleted=(id)=>{
+      setToDoList(
+        toDoList.map((l)=>{
+            if (l.Id===id) {
+                return {...l, Completed:!l.Completed}
+            }else{
+                return l
+            }
+          })
+      )
+    }
   return (
     <div className='toDoList'>
         <h2>TO-DO LIST</h2>
         <div className="top">
             <input onChange={changeHandler} type="text" placeholder='Add Something To Do...' />
-            <button onClick={addHandler}>Add</button>
+            <button className='add' onClick={addHandler}>Add</button>
         </div>
         <div className="bottom">
             <ul className='w-100 px-1'>
                 {
                     toDoList.map((task)=>{
                         return(
-                            <Task Id={task.Id} Name={task.Name} deleteHandler={deleteHandler}/>
+                            <Task key={task.Id} Id={task.Id} Name={task.Name} Completed={task.Completed} deleteHandler={deleteHandler} isCompleted={isCompleted}/>
                         )
                     })
                 }
--- myToDoList/src/components/Task.jsx
+++ myToDoList/src/components/Task.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Task = (props) => {
     return(
-        <li className='d-flex justify-content-between align-items-center py-1' key={props.Id}>
-            <span>{props.Name}</span>
-            <button onClick={()=>props.deleteHandler(props.Id)}>x</button>
+        <li className='d-flex justify-content-between align-items-center py-1'>
+            <span style={props.Completed?{color:"green",textDecoration:"line-through"}:{color:"#000"}}>{props.Name}</span>
+            <div className='d-flex'>
+                <button className='done me-2' onClick={()=>props.isCompleted(props.Id)}><i className="fa-solid fa-check"></i></button>
+                <button className='del' onClick={()=>props.deleteHandler(props.Id)}>x</button>
+            </div>
+            
         </li>
     )
 }
